ea9bcaf14bec5cd0be6746bd0ac6fc0018e6a6e9
Peter Demulsant <peter.demulsant@vizexplorer.com>
2017-03-24 10:11:21 +1300

    update route definition with cleaner syntax

--- template/src/routes/index.js
+++ template/src/routes/index.js
@@ -2,16 +2,12 @@ export default [
     {
         path: '/home',
         name: 'home',
-        component: function (resolve) {
-            require(['@/sections/Home.vue'], resolve)
-        }
+        component: resolve => require(['@/sections/Home.vue'], resolve)
     },
     {
         path: '/about',
         name: 'about',
-        component: function (resolve) {
-            require(['@/sections/About.vue'], resolve)
-        }
+        component: resolve => require(['@/sections/About.vue'], resolve)
     },
     {
         path: '*',
